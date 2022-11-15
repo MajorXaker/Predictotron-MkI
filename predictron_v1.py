e6c753f39284884b1c665775c63b77c652b506a8
Alex Koleda <alxkoleda@gmail.com>
2022-11-16 01:53:19 +0300

    sample predictions

---
+++ predictron_v1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv("music.csv")
+
+X = music_data.drop(columns=["genre"])
+# X - is for input data (convention)
+y = music_data["genre"]
+# y - is for result data (convention)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+score = accuracy_score(y_test, predictions)
+
+aaa = 5
